465401344bc09da3cf5a6c1487b153c6287d3591
Aaron Webb <aaron.webb10@hotmail.com>
2017-11-13 15:09:04 +0000

    modified models

--- todoitem/models.py
+++ todoitem/models.py
@@ -4,5 +4,7 @@ class TodoItem(models.Model):
     name = models.CharField(max_length=30, blank=False)
     done = models.BooleanField(blank=False, default=False)
     
+    
     def __str__(self):
-        return self.name
+        status = "D" if self.done else "N"        
+        return "{0} ({1})".format(self.name, status)
